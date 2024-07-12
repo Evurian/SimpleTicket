d8517ae5b4baec43503a6b6224cb4b71c01cf34e
Evurian <equispemar@unsa.edu.pe>
2024-07-12 17:30:35 -0500

    Implementando envio de correos al momento de registrarss

--- blog_backend/api/account/views.py
+++ blog_backend/api/account/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 from rest_framework.authtoken.models import Token
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 from api.account.serializers import signUpSerializer, signInSerializer
 
@@ -13,8 +16,18 @@ def signUp(request):
     if request.method == 'POST':
         serializer = signUpSerializer(data = request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = serializer.save()
+            
+            # Enviar correo de bienvenida
+            subject = 'Bienvenido a nuestra plataforma'
+            message = f'Hola {user.username},\n\nGracias por registrarte en nuestra plataforma. ¡Esperamos que disfrutes de nuestros servicios!'
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [user.email]
+            
+            send_mail(subject, message, from_email, recipient_list)
+
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+        print(serializer.errors)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
 @api_view(['POST'])
--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -169,3 +169,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
 ]
 """
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587 
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'a9336034@gmail.com'  
+EMAIL_HOST_PASSWORD = 'wruv mzzk nnga qhwj'  
