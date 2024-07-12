3fdf265adc7091cff0f08e87b7986b5c3ac1d7fe
Evurian <139657037+Evurian@users.noreply.github.com>
2024-07-11 19:29:55 -0500

    Backend de la aplicacion

---
+++ blog_backend/api/account/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+
+class signUpSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only = True)
+    confirm_password = serializers.CharField(write_only= True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+            'confirm_password',
+            'email'
+            )
+    
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        validate_password(data['password'])
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+class signInSerializer(serializers.Serializer):
+    
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials.")
---
+++ blog_backend/api/account/views.py
@@ -0,0 +1,28 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+from rest_framework.authtoken.models import Token
+
+from api.account.serializers import signUpSerializer, signInSerializer
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signUp(request):
+    if request.method == 'POST':
+        serializer = signUpSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signIn(request):
+    serializer = signInSerializer(data = request.data)
+    if serializer.is_valid():
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status = status.HTTP_200_OK)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
