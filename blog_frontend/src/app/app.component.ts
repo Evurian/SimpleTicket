54a46a4b853eed4eddc4cbc19079235daab237de
Evurian <equispemar@unsa.edu.pe>
2024-07-17 16:50:00 -0500

    Añadiendo styles a la seccion de productos

--- blog_frontend/src/app/app.component.ts
+++ blog_frontend/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   standalone: true,
