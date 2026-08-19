da83dc9515794345a3a64ec9a7c6745274b602ff
Evurian <equispemar@unsa.edu.pe>
2026-08-19 17:42:20 -0500

    feat: add production environment configuration with API and Google Client ID URLs

--- blog_frontend/src/environments/environment.ts
+++ blog_frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://simpleticket-kshw.onrender.com',
+  apiUrl: 'https://simpleticket-kshw.onrender.com/api/',
   googleClientId: '791530505060-5ce8eda0kbaui09pqvr4o9vk8ag99hf6.apps.googleusercontent.com'
 };
